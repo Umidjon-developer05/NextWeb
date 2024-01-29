c30040d3ba9c0a65b0bda18ba894db899abaf6f3
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-01-29 11:18:18 +0500

    first commit

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes:['/','/courses','/dashbord','/Membership','/BeInstructor','/course-preview/(.*)']
+  publicRoutes:['/','/courses','/course-preview/(.*)']
 });
  
 export const config = {
