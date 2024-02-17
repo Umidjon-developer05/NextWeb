e7ce326098cba755ec5b6e0261d4b7daea5c9335
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-02-17 16:53:13 +0500

    Update page.jsx

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignUp />;
+  return(
+    <section className="bg-white flex justify-center items-center h-[100vh]">
+     <SignUp />
+     </section>
+     );
 }
