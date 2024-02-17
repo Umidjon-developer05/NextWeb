d8cc10ce7202529ced5e40c522a1411267b69b1f
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-02-17 16:43:30 +0500

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
   return  (
-  <section className="bg-white flex justify-center items-center h-[100%]">
+  <section className="bg-white flex justify-center items-center h-[100vh]">
  
         <SignIn/>
   </section>
