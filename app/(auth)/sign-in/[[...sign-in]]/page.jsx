993fb74e44614701d0d9e9e7a516aec48ffc910c
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-02-21 15:07:49 +0500

    as

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,10 +3,6 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return  (
   <section className="bg-white flex justify-center items-center h-[100vh]">
-<<<<<<< HEAD
-=======
- 
->>>>>>> e7ce326098cba755ec5b6e0261d4b7daea5c9335
         <SignIn/>
   </section>
   );
--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -6,8 +6,4 @@ export default function Page() {
      <SignUp />
      </section>
      );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> e7ce326098cba755ec5b6e0261d4b7daea5c9335
