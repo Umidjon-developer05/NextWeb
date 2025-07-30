370a6c2977337c579f1ef213d00341609c44a99e
Umidjon-mobile <umidjoasdasdas@gmail.com>
2025-07-30 18:18:52 +0500

    app

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode:false,
-    images:{
-        domains:['media.graphassets.com']
-    },
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-      },
+  reactStrictMode: false,
+  images: {
+    domains: [
+      "media.graphassets.com",
+      "us-east-1-shared-usea1-02.graphassets.com",
+    ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
